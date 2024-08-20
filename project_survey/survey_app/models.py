7a9cf771e57f8e0e7986dfaef9091c419a51666a
Jacinta <162335518+jhelm1217@users.noreply.github.com>
2024-08-20 18:15:28 -0400

    setting up my models, serializers, views and urls

--- project_survey/survey_app/models.py
+++ project_survey/survey_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Survey(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+class Question(models.Model):
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=255)
+    question_type = models.CharField(max_length=50) 
+
+class Response(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_text = models.TextField()
--- project_survey/survey_app/views.py
+++ project_survey/survey_app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+# from rest_framework import generics
+# from .models import *
+# from .serializers import *
+
+# class SurveyListCreateView(generics.ListCreateAPIView):
+#     queryset = Survey.objects.all()
+#     serializer_class = SurveySerializer
+
+# class SurveyDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Survey.objects.all()
+#     serializer_class = SurveySerializer
+
+# class QuestionListCreateView(generics.ListCreateAPIView):
+#     queryset = Question.objects.all()
+#     serializer_class = QuestionSerializer
+
+# class ResponseCreateView(generics.CreateAPIView):
+#     queryset = Response.objects.all()
+#     serializer_class = ResponseSerializer
